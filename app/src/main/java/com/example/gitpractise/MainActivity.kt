9387a96a305d46a6a93d65b1ea9fb2056bbde699
kunalrjadhav <kunalrjadhav3@gmail.com>
2021-02-12 15:53:07 +0530

    one more commit in MA

--- app/src/main/java/com/example/gitpractise/MainActivity.kt
+++ app/src/main/java/com/example/gitpractise/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gitpractise
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 // in new-feature
+// one more commit
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
