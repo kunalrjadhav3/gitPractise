c613d017c757c745f81019c1379d22b5d048c5f9
kunalrjadhav <kunalrjadhav3@gmail.com>
2021-02-17 12:27:49 +0530

    changes made acc to reviewer

--- app/src/main/java/com/example/gitpractise/MainActivity.kt
+++ app/src/main/java/com/example/gitpractise/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.gitpractise
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-// in new-feature branch
+// in master branch
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
